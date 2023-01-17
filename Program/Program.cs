45efd0b2b1c5a94d943ce3a165a0585f9cb9f47b
Alexandr <guk.zverolis@yandex.ru>
2023-01-17 20:58:06 +0300

    program and diagram commit V1.1

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,3 +1,52 @@
-﻿// Написать программу которая из имеющегося массива строе формирует массив строк, 
-// длина которых рменьше либо равна 3 символа. 
-// Первоначальный массив можно ввести с клавиатуры. Не рекомендуется пользоваться коллекциями.
+﻿/* Написать программу которая из имеющегося массива строк формирует массив строк, 
+ длина которых меньше либо равна 3 символа. 
+ Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+ Не рекомендуется пользоваться коллекциями.
+*/
+
+
+string[] text = {"Hello", "Hi", "Why?", "12,5" ,"yes" ,"no" ,"2022", ":-)","35"};
+string temp = string.Empty;
+int count = 0;
+
+for(int i = 0; i < text.Length; i++)
+{
+   temp = Convert.ToString(text[i]);
+   
+   if(temp.Length < 4)
+   {
+    count++;
+   }
+}
+
+string[] newText = new string[count];
+
+PrintArray(text);
+Console.WriteLine();
+WriteArray(newText);
+PrintArray(newText);
+
+void WriteArray(string[] array)
+{
+    int numbers = 0;
+
+   for(int l = 0; l < text.Length; l++)
+   {
+    temp = Convert.ToString(text[l]);
+  
+        if(temp.Length < 4)
+        {     
+            newText[numbers] = Convert.ToString(temp);
+            numbers++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");   
+    }
+    Console.Write($"{array[array.Length - 1]}");
+}
